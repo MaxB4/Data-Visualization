6b2e2a1a6814696a4c89578c72fc8c9f543d32c5
MaxB4 <max.baneke@hotmail.com>
2019-01-13 15:42:26 +0100

    added different files

--- doc/Data/convertCSVtoJSON.py
+++ doc/Data/convertCSVtoJSON.py
@@ -10,6 +10,6 @@ df = pd.read_excel(INPUT, header=4)
 df = df.dropna()
 
 # turn into JSON file
-# df.set_index('A Centrum').to_json('JSON_file.json', orient='index')
+df.set_index('stadsdeel').to_json('JSON_file.json', orient='index')
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(df)
